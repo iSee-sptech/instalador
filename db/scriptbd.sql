a582e2db41c0d56f2d35c8ae6ef1ee1c3bbfb56e
Mateus Pereira da Silva <99891990+mat-pereira@users.noreply.github.com>
2022-11-28 17:43:12 -0300

    Add files via upload

---
+++ db/scriptbd.sql
@@ -0,0 +1,18 @@
+USE isee;
+
+create table Maquinas (
+serialMaquina varchar(25),
+sistemaOperacionalMaquina varchar(25),
+fabricanteMaquina varchar(25),
+arquiteturaMaquina varchar(25),
+tempoDeAtividade long,
+nomeMaquina varchar (100),
+discoMaquina varchar(10),
+ramMaquina varchar(10),
+processadorMaquina varchar(10),
+cepMaquina char (9),
+numeroMaquina varchar (6),
+pontoDeReferencia varchar (25), 
+imgMaquina varchar (255),
+complementoMaquina varchar (50)
+);
