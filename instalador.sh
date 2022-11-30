4135b2e8fb94e0ba1b232bf7ddabe19b353631bb
Mateus Pereira da Silva <99891990+mat-pereira@users.noreply.github.com>
2022-11-29 21:22:34 -0300

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -59,14 +59,15 @@ fi
             sudo docker build -t dockerfile .
             sudo docker run -d -p 3306:3306 --name ContainerBD -e MYSQL_ROOT_PASSWORD='urubu100' -e MYSQL_DATABASE='isee' dockerfile
     fi
-
+sleep 1
 
 #ABRIR APLICACAO
 
  git clone https://github.com/iSee-sptech/java.git
  cd java
  
-  echo \"[Qual java deseja iniciar? Aperte]\"
+  echo \"[Qual java deseja iniciar? Digite]\"
+  sleep 1
   echo \"[1 - CLI | 2 - Interface Gráfica]\"
   read inst
 	if [ \"$inst\" == \"1\" ]
